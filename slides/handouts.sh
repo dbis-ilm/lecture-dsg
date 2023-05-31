014bda18215fa7356d6a2ee689d8022526cd97b2
Kai-Uwe Sattler <kus@tu-ilmenau.de>
2023-05-31 08:26:41 +0200

    Updates DW and OLAP

--- slides/handouts.sh
+++ slides/handouts.sh
@@ -4,7 +4,8 @@ pdfjam $PARAMS --outfile handouts/dsg-1.pdf main.pdf 1-30
 pdfjam $PARAMS --outfile handouts/dsg-2.pdf main.pdf 31-72
 pdfjam $PARAMS --outfile handouts/dsg-3.pdf main.pdf 73-137
 pdfjam $PARAMS --outfile handouts/dsg-4.pdf main.pdf 142-212
-#pdfjam $PARAMS --outfile handouts/ds-4.pdf main.pdf 272-467
+pdfjam $PARAMS --outfile handouts/dsg-5.pdf main.pdf 213-269
+pdfjam $PARAMS --outfile handouts/dsg-6.pdf main.pdf 270-326
 #pdfjam $PARAMS --outfile handouts/ds-5.pdf main.pdf 468-552
 #pdfjam $PARAMS --outfile handouts/ds-6.pdf main.pdf 553-648
 #pdfjam $PARAMS --outfile handouts/ds-7.pdf main.pdf 655-710
