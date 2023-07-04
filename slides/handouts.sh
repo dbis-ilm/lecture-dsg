e0d217046075226416250dd56d7d71a1d145155a
Kai-Uwe Sattler <kus@tu-ilmenau.de>
2023-07-04 08:02:48 +0200

    Chapter 8: part I

--- slides/handouts.sh
+++ slides/handouts.sh
@@ -6,6 +6,7 @@ pdfjam $PARAMS --outfile handouts/dsg-3.pdf main.pdf 73-137
 pdfjam $PARAMS --outfile handouts/dsg-4.pdf main.pdf 142-212
 pdfjam $PARAMS --outfile handouts/dsg-5.pdf main.pdf 213-269
 pdfjam $PARAMS --outfile handouts/dsg-6.pdf main.pdf 270-326
-pdfjam $PARAMS --outfile handouts/dsg-7.pdf main.pdf 327-392
+pdfjam $PARAMS --outfile handouts/dsg-7.pdf main.pdf 327-435
+pdfjam $PARAMS --outfile handouts/dsg-8.pdf main.pdf 436-489
 #pdfjam $PARAMS --outfile handouts/ds-6.pdf main.pdf 553-648
 #pdfjam $PARAMS --outfile handouts/ds-7.pdf main.pdf 655-710
